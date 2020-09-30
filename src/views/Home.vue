bf54bcc21da120bb9236e063859dc6ea4aae4033
Yang <jy@getutip.com>
2020-09-30 23:01:55 +0200

    Added better korean typer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,17 @@
     <v-row class="text-center d-flex align-center">
       <v-col>
         <div class="console-container">
-          <span id="text"></span>
-          <div class="console-underscore" id="console">&#95;</div>
+          <div>
+            <span id="vue-console-text">
+              {{ showedText }}
+            </span>
+            <div
+              class="console-underscore"
+              :class="{ hidden: !consoleUnderscoreVisible }"
+            >
+              &#95;
+            </div>
+          </div>
         </div>
       </v-col>
     </v-row>
@@ -13,63 +22,115 @@
 
 <script>
 // @ is an alias to /src
-
-// function([string1, string2],target id,[color1,color2])
-
-function consoleText(words, id) {
-  var visible = true;
-  var con = document.getElementById("console");
-  var letterCount = 1;
-  var x = 1;
-  var waiting = false;
-  var target = document.getElementById(id);
-  window.setInterval(function() {
-    if (letterCount === 0 && waiting === false) {
-      waiting = true;
-      target.innerHTML = words[0].substring(0, letterCount);
-      window.setTimeout(function() {
-        var usedWord = words.shift();
-        words.push(usedWord);
-        x = 1;
-        letterCount += x;
-        waiting = false;
-      }, 1000);
-    } else if (letterCount === words[0].length + 1 && waiting === false) {
-      waiting = true;
-      window.setTimeout(function() {
-        x = -1;
-        letterCount += x;
-        waiting = false;
-      }, 1000);
-    } else if (waiting === false) {
-      target.innerHTML = words[0].substring(0, letterCount);
-      letterCount += x;
-    }
-  }, 120);
-  window.setInterval(function() {
-    if (visible === true) {
-      con.className = "console-underscore hidden";
-      visible = false;
-    } else {
-      con.className = "console-underscore";
-
-      visible = true;
-    }
-  }, 400);
-}
+import * as hangul from "hangul-js";
 
 export default {
   name: "Home",
-  mounted() {
-    consoleText(
-      [
-        "I'm Jeong-Heon YANG\n",
-        "Je suis Jeong-Heon YANG",
-        "Ich bin Jeong-Heon YANG",
-        "양정헌입니다"
+  data() {
+    return {
+      consoleText: "",
+      consoleUnderscoreVisible: true,
+      waiting: false,
+      directionReversed: false,
+      x: 0,
+      words: [
+        "양정헌입니다",
+        "I'm Jeong-Heon YANG",
+        "Je suis Jeong-Heon YANG"
       ],
-      "text"
-    );
+      currentWord: "",
+      isKorean: false
+    };
+  },
+  computed: {
+    letterCount() {
+      return this.consoleText.length;
+    },
+    isWordFinished() {
+      return (
+        this.currentWord === "" ||
+        (this.consoleText === "" && this.directionReversed)
+      );
+    },
+    isWordReversable() {
+      return (
+        this.letterCount === this.currentWord.length &&
+        this.waiting === false &&
+        this.x >= this.currentWord.length
+      );
+    },
+    showedText() {
+      if (this.isKorean) {
+        return hangul.assemble(this.consoleText);
+      } else {
+        return this.consoleText;
+      }
+    }
+  },
+  mounted() {
+    window.setInterval(this.activateBotTyper, 120);
+    window.setInterval(this.activateUnderscoreBlink, 400);
+  },
+  destroyed() {
+    clearInterval(this.activateBotTyper);
+    clearInterval(this.activateUnderscoreBlink);
+  },
+  methods: {
+    activateUnderscoreBlink() {
+      this.consoleUnderscoreVisible = !this.consoleUnderscoreVisible;
+    },
+    activateBotTyper() {
+      if (this.waiting) {
+        return;
+      }
+      if (this.isWordFinished) {
+        this.waiting = true;
+        let tmpWord = this.words.shift();
+        let nextWord = "";
+
+        if (
+          tmpWord
+            .charAt(0)
+            .match(
+              /[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/g
+            ) !== null
+        ) {
+          this.isKorean = true;
+          tmpWord = hangul.disassemble(tmpWord);
+
+          tmpWord.forEach(char => {
+            nextWord += char;
+          });
+        } else {
+          nextWord = tmpWord;
+          this.isKorean = false;
+        }
+        this.currentWord = nextWord;
+        this.words.push(this.currentWord);
+        this.reverseDirection();
+      } else if (this.isWordReversable) {
+        this.waiting = true;
+        this.reverseDirection();
+      } else {
+        if (this.directionReversed) {
+          this.consoleText = this.consoleText.slice(0, -1);
+        } else {
+          this.consoleText += this.currentWord.charAt(this.x);
+        }
+        this.x++;
+      }
+    },
+    reverseDirection() {
+      let savedDirection = this.directionReversed;
+      window.setTimeout(() => {
+        if (savedDirection === !this.directionReversed) {
+          return;
+        }
+        this.x = 0;
+        this.directionReversed = !this.directionReversed;
+        this.waiting = false;
+      }, 1000);
+    }
   }
 };
 </script>
